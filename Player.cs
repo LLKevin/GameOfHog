0c22b6ab24498f69d7e4f56c6223b5afb5ae6d4c
Kevin <kevinleo.lucas@gmail.com>
2021-11-12 17:35:27 -0500

    inital commit

---
+++ Player.cs
@@ -0,0 +1,20 @@
+
+using System; 
+
+public class Player {
+
+    public string Name {get; set;}
+    public int Score {get; set;}
+
+    public Player(string name, inte score){
+        Name = name;
+        Score = score;
+    }
+
+
+    private void updatePlayerScore(int score){
+        Score = newScore;
+    }
+
+
+}
---
+++ TurnHandler.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class TurnHandler{
+
+    private bool playerOneTurn = false;
+
+    public TurnHandler(){
+
+    }
+
+
+}
