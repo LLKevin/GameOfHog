fcc6205d59abe3387b9bafdc40c20ce8e052f8dd
Kevin <kevinleo.lucas@gmail.com>
2021-11-17 21:59:18 -0500

    Fix issue preventing the program from running

---
+++ GameOfHog/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfHog
+{
+    internal class Player
+    {
+        internal string Name { get; set; }
+        internal int Score { get; set; }
+
+        Player(string name)
+        {
+            Name = name;
+            Score = 0;
+        }
+
+       void updateScore(int score)
+        {
+            Score = score;      
+        }
+
+    }
+}
---
+++ GameOfHog/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Company.ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
