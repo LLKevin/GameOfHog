9dd1d23f471fddc3cf6b18bf846bc0b97d9bbe09
Kevin <kevinleo.lucas@gmail.com>
2021-11-18 00:12:50 -0500

    add basic menu and dice roll

---
+++ GameOfHog/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfHog
+{
+    public class Dice
+    { 
+       public int roll()
+        {
+
+            //TODO: generate a utility class to generate the pseduo-random number without a library
+            // randomly will select a number fom 1 to 6
+            var rand  = new Random();
+            return rand.Next(1, 6); ; 
+        }
+    }
+}
--- GameOfHog/Program.cs
+++ GameOfHog/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameOfHog;
+using System;
 
 namespace Company.ConsoleApplication1
 {
@@ -6,7 +7,15 @@ namespace Company.ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int selection = 0;
+            while (selection < 3)
+            {
 
+                Console.WriteLine("----Selection----\n Press 1 to start game" +
+                    " \n Press 3 to exit ");
+                selection = Console.Read(); //TODO validation.
+               
+            }
         }
     }
 }
