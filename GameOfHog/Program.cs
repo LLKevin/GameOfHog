fb7ab43718603f39e8ce6d9bd0aaefeaa91ccf93
Kevin <kevinleo.lucas@gmail.com>
2021-11-22 23:34:52 -0500

    Add basic validation for main menu

--- GameOfHog/Program.cs
+++ GameOfHog/Program.cs
@@ -8,23 +8,42 @@ namespace Company.ConsoleApplication1
         static void Main(string[] args)
         {
             int selection = 0;
-            while (selection < 3)
+            string playerName = "";
+            bool validMenuSelection = false;
+
+            while (selection != 3)
             {
 
                 Console.WriteLine("----Selection----\n" +
                     " Press 1 to play game aganist ai \n" +
                     " Press 2 to play game aganist human \n" +
                     " Press 3 to exit the game\n");
-                selection = int.Parse(Console.ReadLine()); //TODO validation &&  Gui with a library
+
+                  validMenuSelection = int.TryParse(Console.ReadLine(), out selection);
+
+                if (validMenuSelection == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You must enter a number to select from the menu\n");
+                }
 
                 switch (selection)
                 {
                     case 1:
                         GameLogic newGame = new GameLogic();
 
-                        // ass for a player name
+                        // ask for a player name
                         Console.WriteLine("What do you want your player to be named?\n");
-                        string playerName = Console.ReadLine();
+                        playerName = Console.ReadLine();
+
+                        while (string.IsNullOrEmpty(playerName)) {
+                            Console.Clear();
+                            Console.WriteLine("You must enter a name\n");
+                            Console.WriteLine("What do you want your player to be named?\n");
+                            playerName = Console.ReadLine();
+                        }
+
+
                         newGame.PlayGameAgainstAi(playerName);
                         break;
                 }
